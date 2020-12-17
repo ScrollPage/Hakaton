59f193f83ec935fb81740ed292d49dee6eae9196
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-12-17 20:20:18 +0300

    no letters

--- backend/detector/api/views.py
+++ backend/detector/api/views.py
@@ -96,8 +96,8 @@ class DetectorListView(ListViewSet):
 
             detectors = _get_annotation()
 
-        content = make_content(detectors, begin_date, end_date, fl)
-        send_report_email.delay(request.user.email, begin_date, end_date, content)
+        # content = make_content(detectors, begin_date, end_date, fl)
+        # send_report_email.delay(request.user.email, begin_date, end_date, content)
         serializer = self.get_serializer(detectors, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
